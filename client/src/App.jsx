cdd52796fa15a89cee7879a897c9f40914c935e5
mattisavec2t <mattis.almeidalima@gmail.com>
2025-10-07 09:26:07 +0200

    refactor: minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./assets/css/common.css"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth.context.jsx";
 import Auth from "./pages/auth.page.jsx";
 import Dashboard from "./pages/dashboard.page.jsx";
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -4,17 +4,12 @@ import {CustomError} from "../errors/custom.error.js";
 export async function hashPassword(password) {
     try {
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        return hashedPassword;
+        return await bcrypt.hash(password, salt);
     } catch (err) {
         throw err;
     }
 }
 
 export async function comparePassword(password, hashedPassword) {
-    try {
-        if (!await bcrypt.compare(password, hashedPassword)) throw new CustomError({ code: 400, message: "Invalid password" });
-    } catch (err) {
-        throw err;
-    }
+    if (!await bcrypt.compare(password, hashedPassword)) throw new CustomError({ code: 400, message: "Invalid password" });
 }
